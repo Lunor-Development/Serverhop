482a9648720bec63489656c703ad1e07e9e374ad
Lunor-Development <kagaruaslays@gmail.com>
2025-12-12 20:08:07 +0700

    Update tpisland2.lua

--- forge/tpisland2.lua
+++ forge/tpisland2.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lunor-Development/Serverhop/refs/heads/main/serverhop.lua"))()(129009554587176)
-if queue_on_teleport then queue_on_teleport("https://raw.githubusercontent.com/Lunor-Development/Serverhop/refs/heads/main/forge/blocktp.lua'))()") end
+if queue_on_teleport then queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Lunor-Development/Serverhop/refs/heads/main/forge/blocktp.lua'))()") end
